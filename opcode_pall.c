5c76fd8b765a5a71d646aa46d0ea735895a44eb8
BarsilNzola <barsiljohn@gmail.com>
2023-08-23 02:34:48 -0700

    pall

---
+++ opcode_pall.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+
+/**
+ * pall - Print all elements of the stack.
+ * @stack: Double pointer to the stack.
+ * @line_number: Line number being executed from the file.
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+    (void)line_number;
+    stack_t *current = *stack;
+
+    while (current)
+    {
+        printf("%d\n", current->n);
+        current = current->next;
+    }
+}
+
