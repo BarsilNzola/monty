c507dd8f6ad44128ebd7f348a6d90b6c140f5a3b
BarsilNzola <barsiljohn@gmail.com>
2023-08-23 02:44:27 -0700

    swap

---
+++ opcode_swap.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+/**
+ * swap - Swap the top two elements of the stack.
+ * @stack: Double pointer to the stack.
+ * @line_number: Line number being executed from the file.
+ */
+void swap(stack_t **stack, unsigned int line_number)
+{
+    if (*stack == NULL || (*stack)->next == NULL)
+    {
+        fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    int temp = (*stack)->n;
+    (*stack)->n = (*stack)->next->n;
+    (*stack)->next->n = temp;
+}
+
